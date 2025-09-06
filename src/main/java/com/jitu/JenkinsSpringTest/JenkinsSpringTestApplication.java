5832ada248327a24242decf1560313e4b5f85fa2
jitendra-jitu <159467483+jitendra-jitu@users.noreply.github.com>
2025-09-06 13:45:43 +0530

    first commit

---
+++ src/main/java/com/jitu/JenkinsSpringTest/JenkinsSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.jitu.JenkinsSpringTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSpringTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jitu/JenkinsSpringTest/JenkinsSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jitu.JenkinsSpringTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
